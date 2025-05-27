23bd8076278dea729dc9f7653299fc60da6483fd
DESKTOP-R12LND5\Serendey <digyrh456789@gmail.com>
2025-05-27 21:46:52 +0900

    BREAKING CHANGE: 기존 디자인, 페이지 다 갈아엎고 새로 만듦

--- port/src/app/page.tsx
+++ port/src/app/page.tsx
@@ -1,9 +1,10 @@
+import Main1 from "@/components/pages/main/main1";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-bluearchive">
-      
+      <Main1/>
     </div>
   );
 }
--- port/src/fonts/blueArchive.ts
+++ port/src/fonts/blueArchive.ts
@@ -1,7 +1,25 @@
 import localFont from 'next/font/local'
 
 export const blueArchive = localFont({
-  src: './blueArchive.otf',
+  src: [
+    {
+      path: './blueArchiveLight.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './blueArchiveMedium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './blueArchive.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
   display: 'swap',
   variable: '--font-bluearchive',
 })
+
+
